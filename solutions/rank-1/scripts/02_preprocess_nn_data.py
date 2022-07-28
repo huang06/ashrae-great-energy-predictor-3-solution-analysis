b86b86332aa87e55c554d676d63f1339f0e44e74
huang06 <tom4996131207@gmail.com>
2022-07-28 17:28:58 +0800

    style: scripts

--- solutions/rank-1/scripts/02_preprocess_nn_data.py
+++ solutions/rank-1/scripts/02_preprocess_nn_data.py
@@ -1,44 +1,64 @@
-import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]=""
-
-import numpy as np 
-import pandas as pd
+import numpy as np
 from ashrae.encoders import FastLabelEncoder
-from ashrae.utils import MODEL_PATH, DATA_PATH, load_data, timer
+from ashrae.utils import DATA_PATH, load_data, timer  # , MODEL_PATH
+
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 
 FEATURES = [
     # building meta features
-    "square_feet", "year_built", "floor_count",
-    
+    "square_feet",
+    "year_built",
+    "floor_count",
     # cat cols
-    "building_id", "site_id", "primary_use", 
-    "hour", "weekday", "weekday_hour",
-    "building_weekday_hour", "building_weekday",
-    "building_hour", 
-    
+    "building_id",
+    "site_id",
+    "primary_use",
+    "hour",
+    "weekday",
+    "weekday_hour",
+    "building_weekday_hour",
+    "building_weekday",
+    "building_hour",
     # raw weather features
-    "air_temperature", "cloud_coverage", "dew_temperature",
-    "precip_depth_1_hr", "sea_level_pressure", "wind_direction", "wind_speed",
-    
+    "air_temperature",
+    "cloud_coverage",
+    "dew_temperature",
+    "precip_depth_1_hr",
+    "sea_level_pressure",
+    "wind_direction",
+    "wind_speed",
     # derivative weather features
-    "air_temperature_mean_lag7", "air_temperature_max_lag7",
-    "air_temperature_min_lag7", "air_temperature_std_lag7",
-    "air_temperature_mean_lag73", "air_temperature_max_lag73",
-    "air_temperature_min_lag73", "air_temperature_std_lag73",
-    
+    "air_temperature_mean_lag7",
+    "air_temperature_max_lag7",
+    "air_temperature_min_lag7",
+    "air_temperature_std_lag7",
+    "air_temperature_mean_lag73",
+    "air_temperature_max_lag73",
+    "air_temperature_min_lag73",
+    "air_temperature_std_lag73",
     # time features
-    "hour_x", "hour_y", "weekday_x", "weekday_y", "is_holiday",
-    
+    "hour_x",
+    "hour_y",
+    "weekday_x",
+    "weekday_y",
+    "is_holiday",
     # target encoding features
-    "gte_meter_building_id_hour", "gte_meter_building_id_weekday",
+    "gte_meter_building_id_hour",
+    "gte_meter_building_id_weekday",
 ]
 
 CAT_COLS = [
-    "building_id", "site_id", "primary_use", 
-    "hour", "weekday", "weekday_hour",
-    "building_weekday_hour", "building_weekday",
-    "building_hour", 
+    "building_id",
+    "site_id",
+    "primary_use",
+    "hour",
+    "weekday",
+    "weekday_hour",
+    "building_weekday_hour",
+    "building_weekday",
+    "building_hour",
 ]
 
 NUM_COLS = [x for x in FEATURES if x not in CAT_COLS]
@@ -47,37 +67,36 @@ NUM_COLS = [x for x in FEATURES if x not in CAT_COLS]
 if __name__ == "__main__":
     """
     python scripts/02_preprocess_nn_data.py
-    """           
+    """
 
     # meter split target normalization
     with timer("Loading data"):
         train, test = load_data("clean")
-        
+
     with timer("Normalize Target"):
         target_encode_cols = [x for x in train.columns if "gte" in x]
-        train[target_encode_cols] = train[target_encode_cols]/np.log1p(train[["square_feet"]].values)
-        test[target_encode_cols] = test[target_encode_cols]/np.log1p(test[["square_feet"]].values)
-        train["target"] = np.log1p(train["meter_reading"])/np.log1p(train["square_feet"])  
-        
-    with timer("Standardize Numeric Features"):    
+        train[target_encode_cols] = train[target_encode_cols] / np.log1p(train[["square_feet"]].values)
+        test[target_encode_cols] = test[target_encode_cols] / np.log1p(test[["square_feet"]].values)
+        train["target"] = np.log1p(train["meter_reading"]) / np.log1p(train["square_feet"])
+
+    with timer("Standardize Numeric Features"):
         for m in range(4):
             train_indices = train.meter == m
-            test_indices = test.meter == m   
-            
-            X = np.concatenate([
-                train.loc[train_indices, NUM_COLS].values,
-                test.loc[test_indices, NUM_COLS].values
-            ])
+            test_indices = test.meter == m
+
+            X = np.concatenate(
+                [train.loc[train_indices, NUM_COLS].values, test.loc[test_indices, NUM_COLS].values]
+            )
             mu = X.mean(0)
             sig = X.std(0)
-            
-            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu)/sig
-            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu)/sig    
-                
-    with timer("Encode Categorical Features"):    
+
+            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu) / sig
+            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu) / sig
+
+    with timer("Encode Categorical Features"):
         for m in range(4):
             train_indices = train.meter == m
-            test_indices = test.meter == m    
+            test_indices = test.meter == m
             for col in CAT_COLS:
 
                 x = np.concatenate([train.loc[train_indices, col], test.loc[test_indices, col]])
@@ -89,33 +108,31 @@ if __name__ == "__main__":
 
     with timer("Save Data"):
         train.to_pickle(f"{DATA_PATH}/preprocessed/train_nn_target_normalized_meter.pkl")
-        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_target_normalized_meter.pkl")    
-
+        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_target_normalized_meter.pkl")
 
     # meter split no normalization
     with timer("Loading data"):
         train, test = load_data("clean")
         train["target"] = np.log1p(train["meter_reading"])
 
-    with timer("Standardize Numeric Features"):    
+    with timer("Standardize Numeric Features"):
         for m in range(4):
             train_indices = train.meter == m
-            test_indices = test.meter == m   
-            
-            X = np.concatenate([
-                train.loc[train_indices, NUM_COLS].values,
-                test.loc[test_indices, NUM_COLS].values
-            ])
+            test_indices = test.meter == m
+
+            X = np.concatenate(
+                [train.loc[train_indices, NUM_COLS].values, test.loc[test_indices, NUM_COLS].values]
+            )
             mu = X.mean(0)
             sig = X.std(0)
-            
-            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu)/sig
-            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu)/sig
 
-    with timer("Encode Categorical Features"):    
+            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu) / sig
+            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu) / sig
+
+    with timer("Encode Categorical Features"):
         for m in range(4):
             train_indices = train.meter == m
-            test_indices = test.meter == m    
+            test_indices = test.meter == m
             for col in CAT_COLS:
 
                 x = np.concatenate([train.loc[train_indices, col], test.loc[test_indices, col]])
@@ -127,8 +144,7 @@ if __name__ == "__main__":
 
     with timer("Save Data"):
         train.to_pickle(f"{DATA_PATH}/preprocessed/train_nn_meter.pkl")
-        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_meter.pkl")        
-
+        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_meter.pkl")
 
     # meter site target normalization
     with timer("Loading data"):
@@ -137,26 +153,25 @@ if __name__ == "__main__":
 
     with timer("Normalize Target"):
         target_encode_cols = [x for x in train.columns if "gte" in x]
-        train[target_encode_cols] = train[target_encode_cols]/np.log1p(train[["square_feet"]].values)
-        test[target_encode_cols] = test[target_encode_cols]/np.log1p(test[["square_feet"]].values)
-        train["target"] = np.log1p(train["meter_reading"])/np.log1p(train["square_feet"])  
+        train[target_encode_cols] = train[target_encode_cols] / np.log1p(train[["square_feet"]].values)
+        test[target_encode_cols] = test[target_encode_cols] / np.log1p(test[["square_feet"]].values)
+        train["target"] = np.log1p(train["meter_reading"]) / np.log1p(train["square_feet"])
 
-    with timer("Standardize Numeric Features"):    
+    with timer("Standardize Numeric Features"):
         for s in range(16):
             train_indices = train.site_id == s
-            test_indices = test.site_id == s 
-            
-            X = np.concatenate([
-                train.loc[train_indices, NUM_COLS].values,
-                test.loc[test_indices, NUM_COLS].values
-            ])
+            test_indices = test.site_id == s
+
+            X = np.concatenate(
+                [train.loc[train_indices, NUM_COLS].values, test.loc[test_indices, NUM_COLS].values]
+            )
             mu = X.mean(0)
             sig = X.std(0)
-            
-            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu)/sig
-            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu)/sig  
 
-    with timer("Encode Categorical Features"):    
+            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu) / sig
+            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu) / sig
+
+    with timer("Encode Categorical Features"):
         for s in range(16):
             train_indices = train.site_id == s
             test_indices = test.site_id == s
@@ -171,29 +186,27 @@ if __name__ == "__main__":
 
     with timer("Save Data"):
         train.to_pickle(f"{DATA_PATH}/preprocessed/train_nn_target_normalized_site.pkl")
-        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_target_normalized_site.pkl")        
-
+        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_target_normalized_site.pkl")
 
     # meter site no normalization
     with timer("Loading data"):
         train, test = load_data("clean")
 
-    with timer("Standardize Numeric Features"):    
+    with timer("Standardize Numeric Features"):
         for s in range(16):
             train_indices = train.site_id == s
-            test_indices = test.site_id == s 
-            
-            X = np.concatenate([
-                train.loc[train_indices, NUM_COLS].values,
-                test.loc[test_indices, NUM_COLS].values
-            ])
+            test_indices = test.site_id == s
+
+            X = np.concatenate(
+                [train.loc[train_indices, NUM_COLS].values, test.loc[test_indices, NUM_COLS].values]
+            )
             mu = X.mean(0)
             sig = X.std(0)
-            
-            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu)/sig
-            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu)/sig
 
-    with timer("Encode Categorical Features"):    
+            train.loc[train_indices, NUM_COLS] = (train.loc[train_indices, NUM_COLS] - mu) / sig
+            test.loc[test_indices, NUM_COLS] = (test.loc[test_indices, NUM_COLS] - mu) / sig
+
+    with timer("Encode Categorical Features"):
         for s in range(16):
             train_indices = train.site_id == s
             test_indices = test.site_id == s
@@ -208,4 +221,4 @@ if __name__ == "__main__":
 
     with timer("Save Data"):
         train.to_pickle(f"{DATA_PATH}/preprocessed/train_nn_site.pkl")
-        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_site.pkl")        
+        test.to_pickle(f"{DATA_PATH}/preprocessed/test_nn_site.pkl")
